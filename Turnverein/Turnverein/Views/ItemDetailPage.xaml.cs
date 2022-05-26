df8edf89772fbb4eb8bfbcb216e5de61fef1695d
Christian Müller <christian.mueller@hfu.ch>
2022-05-26 16:55:12 +0200

    Neues Projekt erstellt

---
+++ Turnverein/Turnverein/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Turnverein.ViewModels;
+using Xamarin.Forms;
+
+namespace Turnverein.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
