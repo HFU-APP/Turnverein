2fb69e4f634b86ae95a7cc613c9d6a8ddd0c2d12
Christian Müller <christian.mueller@hfu.ch>
2022-06-01 17:27:55 +0200

    Class Mitlgieder erstellt

---
+++ Turnverein/Turnverein/Models/Mitglied.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Turnverein.Models
+{
+    internal class Mitglied
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Vorname { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+
+    }
+}
+}
